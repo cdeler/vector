26aefe93a3fde2061a740b43db922820096ebba5
cdeler <serj.krotov@gmail.com>
2017-10-22 13:33:36 -0400

    #2 - change way to valgrind calling (call test directly instead of ctest)

--- utils/Valgrind.cmake
+++ utils/Valgrind.cmake
@@ -1,12 +1,12 @@
 find_program(VALGRIND_PATH valgrind)
 
-function(SETUP_VALGRIND_BUILD _targetname _testrunner)
+function(add_valgrind_check _targetname _test_name)
     if(NOT VALGRIND_PATH)
         message(FATAL_ERROR "valgrind not found! Aborting...")
     endif(NOT VALGRIND_PATH)
 
-    add_custom_target(${_targetname}
-            ${VALGRIND_PATH} --leak-check=full ${_testrunner} --output-on-failure
+    add_custom_target(${_test_name}_valgrind
+            ${VALGRIND_PATH} --leak-check=full --show-reachable=yes --error-exitcode=1 $<TARGET_FILE:${_test_name}>
             )
-
-endfunction(SETUP_VALGRIND_BUILD _targetname _testrunner)
+    add_dependencies(valgrind ${_test_name}_valgrind)
+endfunction(add_valgrind_check _targetname _testrunner)
