150cc81f36649c33636353feac66f8a104b9038f
cdeler <serj.krotov@gmail.com>
2019-03-07 16:15:48 +0300

    Unittests migrated to cmocka

--- src/tests/vector_unittest.c
+++ src/tests/vector_unittest.c
@@ -6,6 +6,9 @@
 #include <stddef.h>
 #include <memory.h>
 #include <stdlib.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <cmocka.h>
 
 #include "vector.h"
 
@@ -13,13 +16,13 @@ static void test_smoke()
 	{
 	Vector *v = vector_open();
 
-	assert(v);
-	assert(vector_getLength(v) == 0);
-	assert(!strcmp((void *) v, VECTOR_EYECATCHER_VALUE));
+	assert_non_null(v);
+	assert_int_equal(0, vector_getLength(v));
+	assert_memory_equal(v, VECTOR_EYECATCHER_VALUE, sizeof(VECTOR_EYECATCHER_VALUE));
 
 	size_t rc = vector_close(&v);
-	assert(v == NULL);
-	assert(rc == 0);
+	assert_null(v);
+	assert_int_equal(0, rc);
 	}
 
 static void test_vector_add()
@@ -31,13 +34,13 @@ static void test_vector_add()
 	char *element = strdup("foo");
 	vector_pushBack(pVector, element);
 
-	assert(vector_getLength(pVector) == 1ULL);
+	assert_int_equal(1, vector_getLength(pVector));
 
 	char *firstElement = vector_elementAt(pVector, 0);
-	assert(!strcmp(firstElement, "foo"));
+	assert_string_equal("foo", firstElement);
 
 	rc = vector_close(&pVector);
-	assert(rc == 1);
+	assert_int_equal(1, rc);
 	}
 
 static void test_removeAt()
@@ -48,27 +51,27 @@ static void test_removeAt()
 	Vector *pVector = vector_open();
 
 	vector_pushBack(pVector, "first");
-	assert(vector_getLength(pVector) == 1);
+	assert_int_equal(1, vector_getLength(pVector));
 	data = vector_removeAt(pVector, 0);
-	assert(data);
-	assert(!strcmp(data, "first"));
-	assert(vector_getLength(pVector) == 0);
+	assert_non_null(data);
+	assert_string_equal("first", data);
+	assert_int_equal(0, vector_getLength(pVector));
 
 	vector_pushBack(pVector, "first");
 	vector_pushBack(pVector, "second");
 	vector_pushBack(pVector, "third");
-	assert(vector_getLength(pVector) == 3);
+	assert_int_equal(3, vector_getLength(pVector));
 	data = vector_removeAt(pVector, 1);
-	assert(data);
-	assert(!strcmp(data, "second"));
-	assert(vector_getLength(pVector) == 2);
-	assert(!strcmp(vector_elementAt(pVector, 0), "first"));
-	assert(!strcmp(vector_elementAt(pVector, 1), "third"));
+	assert_non_null(data);
+	assert_string_equal("second", data);
+	assert_int_equal(2, vector_getLength(pVector));
+	assert_string_equal("first", vector_elementAt(pVector, 0));
+	assert_string_equal("third", vector_elementAt(pVector, 1));
 
-	assert(!strcmp(vector_removeAt(pVector, 1), "third"));
+	assert_string_equal("third", vector_removeAt(pVector, 1));
 
-	assert(vector_getLength(pVector) == 1);
-	assert(!strcmp(vector_elementAt(pVector, 0), "first"));
+	assert_int_equal(1, vector_getLength(pVector));
+	assert_string_equal("first", vector_elementAt(pVector, 0));
 
 	vector_close(&pVector);
 
@@ -77,43 +80,41 @@ static void test_removeAt()
 		{
 		vector_pushBack(pVector, (void *) (0ULL | c));
 		}
-	assert(vector_getLength(pVector) == 10);
+	assert_int_equal(10, vector_getLength(pVector));
 
-	c = (char) vector_removeAt(pVector, 9);
-	assert(c == '9');
+	assert_int_equal('9', (char) vector_removeAt(pVector, 9));
 	static const char expectedVec1[] = "012345678";
 	static const size_t expectedVec1Size = (sizeof(expectedVec1) / sizeof(char)) - 1;
 
-	assert(vector_getLength(pVector) == expectedVec1Size);
+	assert_int_equal(expectedVec1Size, vector_getLength(pVector));
 	for (i = 0; i < expectedVec1Size; ++i)
 		{
-		assert((char) vector_elementAt(pVector, i) == expectedVec1[i]);
+		assert_int_equal(expectedVec1[i], (char) vector_elementAt(pVector, i));
 		}
 
-	assert((char) vector_removeAt(pVector, 0) == '0');
+	assert_int_equal('0', (char) vector_removeAt(pVector, 0));
 	static const char expectedVec2[] = "12345678";
 	static const size_t expectedVec2Size = (sizeof(expectedVec2) / sizeof(char)) - 1;
 
-	assert(vector_getLength(pVector) == expectedVec2Size);
+	assert_int_equal(expectedVec2Size, vector_getLength(pVector));
 	for (i = 0; i < expectedVec2Size; ++i)
 		{
-		assert((char) vector_elementAt(pVector, i) == expectedVec2[i]);
+		assert_int_equal(expectedVec2[i], (char) vector_elementAt(pVector, i));
 		}
 
-	c = (char) vector_removeAt(pVector, 3);
-	assert(c == '4');
+	assert_int_equal('4', (char) vector_removeAt(pVector, 3));
 
 	static const char expectedVec3[] = "1235678";
 	static const size_t expectedVec3Size = (sizeof(expectedVec3) / sizeof(char)) - 1;
 
-	assert(vector_getLength(pVector) == expectedVec3Size);
+	assert_int_equal(expectedVec3Size, vector_getLength(pVector));
 	for (i = 0; i < expectedVec3Size; ++i)
 		{
-		assert((char) vector_elementAt(pVector, i) == expectedVec3[i]);
+		assert_int_equal(expectedVec3[i], (char) vector_elementAt(pVector, i));
 		}
 
-	assert(vector_removeAt(pVector, 5555) == NULL);
-	assert(vector_getLength(pVector) == 7);
+	assert_null(vector_removeAt(pVector, 5555));
+	assert_int_equal(7, vector_getLength(pVector));
 
 	vector_close(&pVector);
 	}
@@ -132,17 +133,17 @@ static void test_complex_with_multiple_realloc()
 		}
 
 	N = vector_getLength(pVector);
-	assert(TEST5_VECTOR_SIZE == N);
+	assert_int_equal(TEST5_VECTOR_SIZE, N);
 
 	for (i = 0; i < TEST5_VECTOR_SIZE; ++i)
 		{
 		val = (size_t) vector_elementAt(pVector, i);
-		assert(val == i);
+		assert_int_equal(i, val);
 		}
 
 	val = vector_close(&pVector);
 
-	assert(val == TEST5_VECTOR_SIZE);
+	assert_int_equal(TEST5_VECTOR_SIZE, val);
 	}
 
 static void test_insertAt()
@@ -157,53 +158,55 @@ static void test_insertAt()
 		vector_pushBack(pVector, (void *) i);
 		}
 
-	assert(vector_getLength(pVector) == 9);
+	assert_int_equal(9, vector_getLength(pVector));
 
 	rc = vector_insertAt(pVector, 9, (void *) 0xDEADBEEF);
 
-	assert(rc == 0);
+	assert_int_equal(0, rc);
 
 	static const size_t exectedArr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 0xDEADBEEF};
 	static const size_t exectedArr1Size = sizeof(exectedArr1) / sizeof(size_t);
-	assert(vector_getLength(pVector) == exectedArr1Size);
 
+	assert_int_equal(exectedArr1Size, vector_getLength(pVector));
 	for (i = 0; i < exectedArr1Size; ++i)
 		{
-		assert(vector_elementAt(pVector, i) == (void *) exectedArr1[i]);
+		assert_int_equal(exectedArr1[i], vector_elementAt(pVector, i));
 		}
 
 	rc = vector_insertAt(pVector, 5, (void *) 0xCAFEBABE);
-	assert(rc == 0);
+	assert_int_equal(0, rc);
 
 	static const size_t exectedArr2[] = {0, 1, 2, 3, 4, 0xCAFEBABE, 5, 6, 7, 8, 0xDEADBEEF};
 	static const size_t exectedArr2Size = sizeof(exectedArr2) / sizeof(size_t);
-	assert(vector_getLength(pVector) == exectedArr2Size);
 
+	assert_int_equal(exectedArr2Size, vector_getLength(pVector));
 	for (i = 0; i < exectedArr2Size; ++i)
 		{
-		assert(vector_elementAt(pVector, i) == (void *) exectedArr2[i]);
+		assert_int_equal(exectedArr2[i], vector_elementAt(pVector, i));
 		}
 
 	rc = vector_insertAt(pVector, 0x100, (void *) 0x100500);
-	assert(rc < 0);
 
-	assert(vector_getLength(pVector) == exectedArr2Size);
+	assert_true(rc < 0);
+	assert_int_equal(exectedArr2Size, vector_getLength(pVector));
 	for (i = 0; i < exectedArr2Size; ++i)
 		{
-		assert(vector_elementAt(pVector, i) == (void *) exectedArr2[i]);
+		assert_int_equal(exectedArr2[i], vector_elementAt(pVector, i));
 		}
-
 
 	vector_close(&pVector);
 	}
 
 int main()
 	{
-	test_smoke();
-	test_vector_add();
-	test_removeAt();
-	test_complex_with_multiple_realloc();
-	test_insertAt();
+	static const struct CMUnitTest tests[] =
+			{
+					cmocka_unit_test(test_smoke),
+					cmocka_unit_test(test_vector_add),
+					cmocka_unit_test(test_removeAt),
+					cmocka_unit_test(test_complex_with_multiple_realloc),
+					cmocka_unit_test(test_insertAt),
+			};
 
-	return 0;
+	return cmocka_run_group_tests_name("vector", tests, NULL, NULL);
 	}
